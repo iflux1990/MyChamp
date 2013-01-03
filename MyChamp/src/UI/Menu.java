8845d570815c838702f564235ef59188239d537a
iFluX1990 <dalle@iflux.dk>
2013-01-03 12:26:30 +0100

    TeamManager implemented

--- MyChamp/src/UI/Menu.java
+++ MyChamp/src/UI/Menu.java
@@ -10,35 +10,5 @@ package UI;
  */
 public class Menu
 {
-    private int EXIT_OPTION;
- 
-    public void run()
-    {
-        boolean done = false;
-        while (!done)
-        {
-            showMenu();
-            int option = getOption();
-            doAction(option);
-            if (option == EXIT_OPTION)
-            {
-                done = true;
-            }
-        }
-    }
-
-    private void showMenu()
-    {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    private int getOption()
-    {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    private void doAction(int option)
-    {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+    
 }
