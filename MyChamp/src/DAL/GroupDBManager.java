fb99190a51c28e3214b87ca91e7af58d830b5d9e
Makja <mak.j@hotmail.com>
2013-01-04 14:31:12 +0100

    addteam, listall og update skulle gerne virke

--- MyChamp/src/DAL/GroupDBManager.java
+++ MyChamp/src/DAL/GroupDBManager.java
@@ -5,18 +5,50 @@
 package DAL;
 
 import BE.Group;
+import BE.Team;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
  *
  * @author Daniel
  */
-public class GroupDBManager
+public class GroupDBManager extends ConnectionDBManager
 {
 
-    public ArrayList<Group> listAll()
+    public GroupDBManager() throws IOException
     {
-        throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
+    public ArrayList<Group> listAllGroups() throws  SQLException
+            {
+        Connection con = dataSource.getConnection();
+
+        String sql = "SELECT * FROM Group";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+
+
+        ArrayList<Group> group = new ArrayList<>();
+        while (rs.next())
+        {
+
+            int ID = rs.getInt("ID");
+            String GroupName = rs.getString("School");
+            String teamcaptain = rs.getString("TeamCaptain");
+            String email = rs.getString("Email");
+//            int groupid = rs.getInt("GroupID");
+//            int points = rs.getInt("Points");
+
+
+            Group g = new Group(ID, GroupName);
+            group.add(g);
+        }
+        return group;
+    }
 }
