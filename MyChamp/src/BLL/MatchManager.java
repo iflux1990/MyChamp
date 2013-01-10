1139c4a62a71126c288cb6364527e9f9433fd523
Makja <mak.j@hotmail.com>
2013-01-10 09:59:29 +0100

    Matches kan blive lavet med 15 hold også

--- MyChamp/src/BLL/MatchManager.java
+++ MyChamp/src/BLL/MatchManager.java
@@ -44,11 +44,11 @@ public class MatchManager
                     if (group1.get(i) != group1.get(j))
                     {
 //                      System.out.println(group1.get(i).getSchoolName() + " vs " + group1.get(j).getSchoolName());
-                        Match m = new Match(1, 1, group1.get(i).getTeamId(), group1.get(j).getTeamId());
-                        mdb.addMatches(m);
+                        Match m1 = new Match(1, 1, group1.get(i).getTeamId(), group1.get(j).getTeamId());
+                        mdb.addMatches(m1);
+
                     }
                 }
-                System.out.println("");
             }
         }
 
@@ -62,8 +62,9 @@ public class MatchManager
                     if (group2.get(i) != group2.get(j))
                     {
 //                      System.out.println(group1.get(i).getSchoolName() + " vs " + group1.get(j).getSchoolName());
-                        Match m = new Match(1, 1, group2.get(i).getTeamId(), group2.get(j).getTeamId());
-                        mdb.addMatches(m);
+                        Match m2 = new Match(1, 1, group2.get(i).getTeamId(), group2.get(j).getTeamId());
+                        mdb.addMatches(m2);
+
 
                     }
                 }
@@ -80,8 +81,9 @@ public class MatchManager
                     if (group3.get(i) != group3.get(j))
                     {
 //                      System.out.println(group1.get(i).getSchoolName() + " vs " + group1.get(j).getSchoolName());
-                        Match m = new Match(1, 1, group3.get(i).getTeamId(), group3.get(j).getTeamId());
-                        mdb.addMatches(m);
+                        Match m3 = new Match(1, 1, group3.get(i).getTeamId(), group3.get(j).getTeamId());
+                        mdb.addMatches(m3);
+
                     }
                 }
             }
@@ -99,13 +101,18 @@ public class MatchManager
                         if (group4.get(i) != group4.get(j))
                         {
 //                      System.out.println(group1.get(i).getSchoolName() + " vs " + group1.get(j).getSchoolName());
-                            Match m = new Match(1, 1, group4.get(i).getTeamId(), group4.get(j).getTeamId());
-                            mdb.addMatches(m);
+                            Match m4 = new Match(1, 1, group4.get(i).getTeamId(), group4.get(j).getTeamId());
+                            mdb.addMatches(m4);
+
                         }
                     }
                 }
             }
         }
-
+       
+ System.out.println("Match 1: " + group1.get(0) + " vs " + group1.get(1));
+        System.out.println("Match 2: " + group1.get(2) + " vs " + group1.get(3));
     }
 }
+
+
